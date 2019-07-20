61a4c2bc8e4acca2aa31247206e609ef17dce75c
tarekgabarin <tarekgabarin1992@gmail.com>
2019-07-20 17:01:06 -0400

    added start button to start menu

--- src/components/StartMenu/StartMenu.js
+++ src/components/StartMenu/StartMenu.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import './StartMenu.css'
 
 class StartMenu extends Component {
     render() {
         return (
             <div>
-                <h1>Start Typing Test</h1>
-
+                <div>
+                    <h1>Start Typing Test</h1>
+                </div>
                 <div className="styled-select semi-square" >
                     <select>
                         <option>1 minute</option>
@@ -15,6 +15,12 @@ class StartMenu extends Component {
                         <option>4 minutes</option>
                         <option>5 minutes</option>
                     </select>
+                </div>
+
+                <div className="styled-button">
+                    <button className={'rounded'} >
+                        Start
+                    </button>
                 </div>
 
 
