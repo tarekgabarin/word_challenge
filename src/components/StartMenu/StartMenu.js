1c73f72b7417414f4e80e05bb5687cc14fc399b9
tarekgabarin <tarekgabarin1992@gmail.com>
2019-07-23 12:35:47 -0400

    added css spacing

--- src/components/StartMenu/StartMenu.js
+++ src/components/StartMenu/StartMenu.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 class StartMenu extends Component {
+    
     render() {
         return (
             <div>
-                <div>
+                <div className={'item'}>
                     <h1>Start Typing Test</h1>
                 </div>
-                <div className="styled-select semi-square" >
+                <div className="styled-select semi-square item" >
                     <select>
                         <option>1 minute</option>
                         <option>2 minutes</option>
@@ -17,7 +18,7 @@ class StartMenu extends Component {
                     </select>
                 </div>
 
-                <div className="styled-button">
+                <div className="styled-button item">
                     <button className={'rounded'} >
                         Start
                     </button>
