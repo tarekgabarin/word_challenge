b6576a1f27285a8fdd8a94711077a0fdab77552d
tarekgabarin <tarekgabarin1992@gmail.com>
2019-07-23 13:03:19 -0400

    added select dropdown functionality

--- src/components/StartMenu/StartMenu.js
+++ src/components/StartMenu/StartMenu.js
@@ -1,20 +1,66 @@
 import React, {Component} from 'react';
 
 class StartMenu extends Component {
-    
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedTimeInSeconds: 60,
+            listOfTimes: [
+                {
+                text: '1 minute',
+                time: 60
+                },
+                {
+                    text: '2 minute',
+                    time: 120
+                },
+                {
+                    text: '3 minute',
+                    time: 180
+                },
+                {
+                    text: '4 minute',
+                    time: 240
+                },
+                {
+                    text: '5 minute',
+                    time: 300
+                },
+            ]
+
+        }
+
+    }
+
+    onSelectTime = (event) => {
+
+        const selectedTime = Number(event.target.value);
+
+        this.setState({
+            selectedTimeInSeconds: selectedTime
+        });
+
+        console.log('selectedTime is', selectedTime);
+
+    };
+
     render() {
+
+        const timeOptionsJSX = this.state.listOfTimes.map(item => {
+            return <option value={item.time}>{item.text}</option>
+        });
+
+
         return (
             <div>
                 <div className={'item'}>
                     <h1>Start Typing Test</h1>
                 </div>
                 <div className="styled-select semi-square item" >
-                    <select>
-                        <option>1 minute</option>
-                        <option>2 minutes</option>
-                        <option>3 minutes</option>
-                        <option>4 minutes</option>
-                        <option>5 minutes</option>
+                    <select value={this.state.selectedTimeInSeconds} onChange={(e) => this.onSelectTime(e)}>
+                        {timeOptionsJSX}
                     </select>
                 </div>
 
