0d3754bb66b52bf01ccd6b4f3af791886addb41e
tarekgabarin <tarekgabarin1992@gmail.com>
2019-07-25 13:16:06 -0400

    removed unneeeded dependencies files and commentts

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from './config.js'
+
 
 export function getWord() {
     return axios.get('https://random-word-api.netlify.com/.netlify/functions/word?key=foobar');
